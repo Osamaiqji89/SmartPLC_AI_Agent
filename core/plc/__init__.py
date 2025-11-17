23511f06ee1787be2e9ff2ed011c12ba337673bc
Sultan <147691587+Osamaiqji89@users.noreply.github.com>
2025-11-17 10:59:53 +0100

    Add __init__.py files for core packages and GUI widgets with module descriptions

---
+++ core/plc/__init__.py
@@ -0,0 +1,1 @@
+"""PLC simulation package"""
---
+++ gui/widgets/__init__.py
@@ -0,0 +1,8 @@
+"""
+Custom Widgets Package
+"""
+from gui.widgets.LiveIndicator import LiveIndicator
+from gui.widgets.BarChartWidget import BarChartWidget
+from gui.widgets.GaugeWidget import GaugeWidget
+
+__all__ = ['LiveIndicator', 'BarChartWidget', 'GaugeWidget']
